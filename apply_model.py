8674b57260c632bcf3cb552a5f76d7988fc49ed1
Riley Conroy <rpconroy@ucar.edu>
2021-07-15 15:36:55 -0600

    Minor documentation and refactoring changes

--- apply_model.py
+++ apply_model.py
@@ -1,7 +1,8 @@
 #!/glade/u/home/jdubeau/github/rda-ML-resource/venv/bin/python3
-# Full machine learning process, start to finish:
-# Takes in an rindex number and request type, and uses the current best 
-# ML models to predict the time and the request will use.
+"""Full machine learning process, start to finish:
+Takes in an rindex number and request type, and uses the current best
+ML models to predict the time and the request will use.
+"""
 import cProfile
 import pstats
 from pstats import SortKey
@@ -24,45 +25,60 @@ logging.basicConfig(filename=log_file,
                     level=logging.INFO,
                     datefmt='%Y-%m-%d %H:%M:%S')
 
-def get_settings():
+def get_settings(config_file="/glade/u/home/jdubeau/github/rda-ML-resource/model-config.json"):
+    """Loads settings from a file.
+
+    Parameters
+    ----------
+        config_file (str): File to load.
+
+    Returns:
+        None
+    """
     global settings
-    config_file = open("/glade/u/home/jdubeau/github/rda-ML-resource/model-config.json")
+    config_file = open()
     settings = json.load(config_file)
     config_file.close()
 
 def read_args():
-    '''Reads command-line arguments and returns them, after translating the
+    """Reads command-line arguments and returns them, after translating the
     request type from 'P', 'R', or 'S' to 'PP', 'BR', or 'SP' respectively.
-    
+
     The given rindex must be an integer and the given request type must
     be one of P, R, S.
-    '''
+
+    Returns:
+        (tuple): (rindex, request_type)
+    """
     rtype_dict = {'P':'PP', 'R':'BR', 'S':'SP'}
-    
+
     rindex = int(sys.argv[1])
     try:
         request_type = rtype_dict[sys.argv[2]]
     except (IndexError, KeyError):
         logging.warning("Missing or invalid request type. Default request type SP assumed.")
         request_type = 'SP'
-    
+
     return rindex, request_type
 
 def get_rinfo(rindex):
-    '''Gets the rinfo string associated to the given rindex by connecting
+    """Gets the rinfo string associated to the given rindex by connecting
     to the dsrqst table and performing a mysql query.
-    
+
     Parameters
     ----------
     rindex : int
         rindex number to look up.
-    '''
+
+    returns
+        (str) rinfo
+    """
     credentials_path = '/glade/u/home/jdubeau/github/rda-ML-resource/dsrqst-creds.json'
     credentials = json.load(open(credentials_path))
-        
+
     host_name = 'rda-db.ucar.edu'
     db_name = 'dssdb'
-    
+
     conn = mysql.connector.connect(user=credentials['user'],
                                    password=credentials['password'],
                                    host=host_name,
@@ -70,9 +86,9 @@ def get_rinfo(rindex):
     curs = conn.cursor()
     curs.execute("SELECT rinfo FROM dsrqst WHERE rindex = %s",
                  (rindex,))
-    
+
     rinfo = curs.fetchone()[0]
-   
+
     conn.close()
     return rinfo
 
@@ -81,11 +97,13 @@ def handle_missing_rinfo_val(feature):
     not present in an rinfo string. This function only handles missing
     basic rinfo values, like 'elon' and 'wlon' -- for missing composite
     values like 'rqst_area_rect', see handle_missing in preprocessing.py.
-    
+
     Parameters
     ----------
     feature : str
         Name of feature to decide null value for.
+
+    Returns (bool or pd.NaT or NaN): The null value when a feature is missing
     """
 
     if feature == 'gui':
@@ -94,19 +112,19 @@ def handle_missing_rinfo_val(feature):
         return pd.NaT
     else:
         return float('nan')
-    
+
 
 def format_rinfo_val(rinfo, feature, val):
     """Converts a value found in an rinfo string to the correct data type
     so that it can be entered into the pandas dataframe.
-    
-    Example input: 
+
+    Example input:
     rinfo = '...;elon=76.4;...'
     feature = 'elon'
     val = '76.4'
 
     Example output: 76.4 (type float)
-    
+
     Parameters
     ----------
     rinfo : str
@@ -134,19 +152,19 @@ def format_rinfo_val(rinfo, feature, val):
     else:
         return val
 
-    
+
 def get_rinfo_val(rinfo, feature, rinfo_feature_names):
     """Finds the value of the given feature in the given rinfo string.
     If the feature is not present in rinfo, calls handle_missing_rinfo_val.
-    If the feature is present, uses format_rinfo_val to convert the value 
+    If the feature is present, uses format_rinfo_val to convert the value
     to the appropriate data type before returning.
-    
-    Example input: 
-    rinfo = '...;elon=76.4;...' 
+
+    Example input:
+    rinfo = '...;elon=76.4;...'
     feature = 'elon'
 
     Example output: '76.4'
-    
+
     Parameters
     ----------
     rinfo : str
@@ -157,9 +175,9 @@ def get_rinfo_val(rinfo, feature, rinfo_feature_names):
         Dictionary relating each feature name to a list of alternate
         names for the same feature.
     """
-    
+
     rinfo = rinfo.replace('%3D', '=')
-    
+
     if ';' in rinfo:
         sep = ';'
     else:
@@ -174,7 +192,7 @@ def get_rinfo_val(rinfo, feature, rinfo_feature_names):
                 val = rinfo[start_ind:end_ind]
             else:
                 val = rinfo[start_ind:]
-                
+
     val = format_rinfo_val(rinfo, feature, val)
     return val
 
@@ -183,7 +201,7 @@ def valid_rinfo(rinfo):
     """Currently unused.
     Decides whether an rinfo string is 'valid.'
     In practice, just serves to filter out a few problematic rinfo strings.
-    
+
     Parameters
     ----------
     rinfo : str
@@ -197,15 +215,15 @@ def valid_rinfo(rinfo):
         return False
     else:
         return True
-    
+
 
 def parse_lats_lons(val):
-    """Takes a 'lats' or 'lons' value and returns two floats representing the 
+    """Takes a 'lats' or 'lons' value and returns two floats representing the
     southern/western coordinate and the northern/eastern coordinate.
-    
+
     Example input: '60 S 80 N'
     Example output: (-60.0, 80.0)
-    
+
     Parameters
     ----------
     val : str
@@ -230,14 +248,14 @@ def parse_lats_lons(val):
 
 
 def update_lat_lon(feature, row):
-    """Used to update 'slat', 'nlat', 'wlon', or 'elon' by 
+    """Used to update 'slat', 'nlat', 'wlon', or 'elon' by
     getting the values from 'lats' or 'lons' in the same row.
-    
-    Example input: 
+
+    Example input:
     feature = 'nlat'
     row = <row in which row['lats'] = '45 S 50 N'>
     Example output: 50.0
-    
+
     Parameters
     ----------
     feature : str
@@ -245,12 +263,12 @@ def update_lat_lon(feature, row):
     row : pandas.core.series.Series
         Row of dataframe to be updated.
     """
-    # First check to see if there is a non-null value for 
+    # First check to see if there is a non-null value for
     # 'lats' or 'lons' in the row. The two always come together,
     # so if suffices to just check for one of them.
     if row['lats'] != row['lats']:
         return row[feature]
-    
+
     else:
         if feature == 'slat':
             return parse_lats_lons(row['lats'])[0]
@@ -260,18 +278,18 @@ def update_lat_lon(feature, row):
             return parse_lats_lons(row['lons'])[0]
         else:
             return parse_lats_lons(row['lons'])[1]
-        
+
 
 def parse_dates(feature, dates):
     """Deduces a start date or end date from whatever was in
-    the 'dates' column. The entered feature must be either 
+    the 'dates' column. The entered feature must be either
     'startdate' or 'enddate'.
-    
-    Example input: 
+
+    Example input:
     feature = 'enddate'
     dates = '2019-01-01 00:00 2019-12-31 18:00'
     Example output: 2019-12-31 18:00 (pandas datetime object)
-    
+
     Parameters
     ----------
     feature : str
@@ -280,7 +298,7 @@ def parse_dates(feature, dates):
         Content of 'dates' column.
     """
     dates_split = dates.split(' ')
-        
+
     if len(dates_split) == 4:
         # Typical case: dates=2019-01-01 00:00 2019-12-31 18:00
         if feature == 'startdate':
@@ -294,7 +312,7 @@ def parse_dates(feature, dates):
             date = dates_split[0]
         else:
             date = dates_split[1]
-            
+
         if '-' not in dates_split[0]:
             date = date[:4] + '-' + date[4:]
     try:
@@ -306,7 +324,7 @@ def parse_dates(feature, dates):
 def update_dates(feature, row):
     """Used to update 'startdate', 'enddate', or 'dates_init' by
     getting the values from the 'dates' column in the same row.
-    
+
     Parameters
     ----------
     feature : str
@@ -315,7 +333,7 @@ def update_dates(feature, row):
         Row to be updated.
     """
     dates = row['dates']
-    
+
     if feature == 'dates_init':
         return True if dates == 'init' else False
 
@@ -326,34 +344,34 @@ def update_dates(feature, row):
             return parse_dates(feature, dates)
         else:
             return pd.NaT
-        
+
 
 def custom_predict(X, model, threshold=0.7):
-    '''Custom method of making predictions based on the model's probability 
+    """Custom method of making predictions based on the model's probability
     estimates. Finds the three most likely guesses and selects the highest
     class, unless the model is very sure about its first guess. Returns
     an array of predicted classes.
-    
+
     Parameters
     ----------
     X : numpy.ndarray of shape (n_entries, n_features)
         2-D array of entries to predict.
-    model : sklearn.ensemble._forest.RandomForestClassifier 
+    model : sklearn.ensemble._forest.RandomForestClassifier
             or other sklearn classifier
         Fitted model to get predictions from.
-    '''
+    """
     probas = model.predict_proba(X)
     preds = []
     for entry in probas:
         first_guess = np.argmax(entry)
         first_proba = np.amax(entry)
-        
+
         entry[first_guess] = 0
         second_guess = np.argmax(entry)
-        
+
         entry[second_guess] = 0
         third_guess = np.argmax(entry)
-        
+
         if first_proba > threshold:
             preds.append(first_guess)
         else:
@@ -363,16 +381,16 @@ def custom_predict(X, model, threshold=0.7):
 
 
 def translate_predictions(preds, categories_dict, init_val=3.0, ten_pct_pt=10000):
-    '''Translates predictions from categories into actual values.
-    
+    """Translates predictions from categories into actual values.
+
     First interprets the predicted category as a value using the
     given dictionary.
 
     Then multiplies that amount by a scaling factor which begins
     at the given init_val and decays for higher memory values,
-    reaching a value of 1.1 (thus scaling by 10%) at the 
-    given value called ten_pct_pt. 
-       
+    reaching a value of 1.1 (thus scaling by 10%) at the
+    given value called ten_pct_pt.
+
     Parameters
     ----------
     preds : numpy.ndarray of shape (n_entries,)
@@ -383,12 +401,12 @@ def translate_predictions(preds, categories_dict, init_val=3.0, ten_pct_pt=10000
         Initial scaling value. Values close to zero will be scaled up by
         approximately this amount.
     ten_pct_pt : int (default 10000)
-        Ten percent point. Values near the ten percent point will be 
+        Ten percent point. Values near the ten percent point will be
         scaled up by approximately 10%.
-    '''
+    """
     a = init_val - 1
     b = (1/ten_pct_pt)*math.log(0.1/a)
-    
+
     scaled_preds = []
     for category in preds:
         val = categories_dict[category]
@@ -399,11 +417,11 @@ def translate_predictions(preds, categories_dict, init_val=3.0, ten_pct_pt=10000
     return scaled_preds
 
 def get_rinfo_feature_names():
-    '''Determines which features will be parsed from the rinfo string,
-    and what other possible names those features could have. Returns 
+    """Determines which features will be parsed from the rinfo string,
+    and what other possible names those features could have. Returns
     a dictionary mapping each feature name to a list of possible
     feature names representing the same value.
-    '''
+    """
     all_rinfo_features = ['dates', 'dsnum', 'elon', 'enddate',
                           'format', 'grid_definition', 'gui',
                           'lats', 'level', 'lons', 'nlat',
@@ -414,54 +432,54 @@ def get_rinfo_feature_names():
                          'parameters': ['parameters', 'params', 'parms', 'vars'],
                          'format': ['format', 'ofmt', 'fmt'],
                          'tindex': ['tindex', 'gindex']}
-    
+
     normal_features = [feat for feat in all_rinfo_features
                        if feat not in special_alt_names]
 
     rinfo_feature_names = {feat: [feat] for feat in normal_features}
     rinfo_feature_names.update(special_alt_names)
-    
+
     return rinfo_feature_names
 
 
 def add_rinfo_features(df):
-    '''Takes a dataframe with an rinfo column and adds all features that
-    can be parsed out of the column. 
-    
+    """Takes a dataframe with an rinfo column and adds all features that
+    can be parsed out of the column.
+
     Parameters
     ----------
     df : pandas.core.frame.DataFrame
         Dataframe with rinfo column.
-    '''
+    """
     rinfo_feature_names = get_rinfo_feature_names()
 
     for feature in rinfo_feature_names:
-        df[feature] = df.apply(lambda row: 
-                               get_rinfo_val(row['rinfo'], 
-                                             feature, 
+        df[feature] = df.apply(lambda row:
+                               get_rinfo_val(row['rinfo'],
+                                             feature,
                                              rinfo_feature_names),
                                axis = 1)
 
     return df
 
-def combine_redundant_features(df):    
-    '''Separates the information from columns 'lats'/'lons' 
-    and 'dates' into the columns ('slat', 'nlat', 'wlon', 'elon') 
+def combine_redundant_features(df):
+    """Separates the information from columns 'lats'/'lons'
+    and 'dates' into the columns ('slat', 'nlat', 'wlon', 'elon')
     and ('startdate', 'enddate', 'dates_init'), respectively.
-    Drops the now-unnecessary 'lats', 'lons', and 'dates' columns and 
+    Drops the now-unnecessary 'lats', 'lons', and 'dates' columns and
     renames 'startdate' and 'enddate' to 'rqst_start_at' and 'rqst_end_at'.
-    
+
     Parameters
     ----------
     df : pandas.core.frame.DataFrame
         Dataframe to process.
-    '''
+    """
     for feature in ['slat', 'nlat', 'wlon', 'elon']:
         df[feature] = df.apply(lambda row: update_lat_lon(feature, row), axis = 1)
 
     for feature in ['dates_init', 'startdate', 'enddate']:
         df[feature] = df.apply(lambda row: update_dates(feature, row), axis = 1)
-    
+
     df.drop(labels=['lats', 'lons', 'dates'], axis=1, inplace=True)
 
     df.rename(columns={'enddate': 'rqst_end_at',
@@ -470,12 +488,12 @@ def combine_redundant_features(df):
     return df
 
 def process_with_model(df, X_features, X_train):
-    '''Preprocess the data according to how the model was made.
+    """Preprocess the data according to how the model was made.
     I.e. add any features the model uses that aren't already there,
-    and scale the data the same way the model's input data was 
+    and scale the data the same way the model's input data was
     scaled in training. Returns a numpy.ndarray with the processed
     data, ready for the model to predict.
-    
+
     Parameters
     ----------
     df : pandas.core.frame.DataFrame
@@ -484,75 +502,75 @@ def process_with_model(df, X_features, X_train):
         List of feature names the model uses.
     X_train: pandas.core.frame.DataFrame
         Training set the model used (unscaled).
-    '''
+    """
     new_features = [feat for feat in X_features if feat not in df.columns]
     df = add_new_features(df, new_features)
-    
+
     df = handle_missing(df)
 
     X = df[X_features]
     X = scale_other(X, X_train)
-    
+
     return X
 
 def predict_with_model(X, model, categories_dict, target):
-    '''Use the model to predict either the wall time or the used
+    """Use the model to predict either the wall time or the used
     memory for the given (already processed) input data.
-    
+
     Parameters
     ----------
     X : pandas.core.frame.DataFrame
         Processed input data.
-    model : sklearn.ensemble._forest.RandomForestClassifier 
+    model : sklearn.ensemble._forest.RandomForestClassifier
             or other sklearn classifier
         Fitted model to get predictions from.
     target : str
         Either 'mem' or 'time'.
-    '''
+    """
     pred_categories = custom_predict(X, model)
-    
+
     if target == 'mem':
-        predictions = translate_predictions(pred_categories, 
+        predictions = translate_predictions(pred_categories,
                                         categories_dict,
                                         **settings['mem_scaling'])
     else:
-        predictions = translate_predictions(pred_categories, 
+        predictions = translate_predictions(pred_categories,
                                         categories_dict,
                                         **settings['time_scaling'])
-    
+
     return predictions
 
 def format_predictions(mem, time):
-    '''Formats predictions into a PBS formatted resource request string.
-    
+    """Formats predictions into a PBS formatted resource request string.
+
     Parameters
     ----------
     mem : int
         Number of megabytes predicted.
     time : int
         Number of seconds predicted.
-    '''
+    """
     mem_string = str(mem)+'mb'
-    
+
     time_string = str(pd.to_timedelta(time, unit='S'))[-8:]
-    
+
     output_string = f"-l walltime={time_string},select=1:mem={mem_string}"
-    
+
     return output_string
 
 def main():
     get_settings()
     save_paths = settings['model_paths']
-    
+
     rindex, request_type = read_args()
     rinfo = get_rinfo(rindex)
 
     input_df = pd.DataFrame([[request_type, rinfo]], columns=['request_type', 'rinfo'])
     input_df = add_rinfo_features(input_df)
     input_df = combine_redundant_features(input_df)
-    
+
     predictions = []
-    
+
     for target in ['mem', 'time']:
         ms = model_saver()
         ms.load(save_paths[target])
@@ -560,10 +578,10 @@ def main():
         X = process_with_model(input_df, X_features, X_train)
         pred = predict_with_model(X, model, categories_dict, target)
         predictions.append(pred)
-    
+
     predictions = np.ravel(predictions)
     print(format_predictions(predictions[0], predictions[1]))
-    
+
 
 if __name__ == "__main__":
     try:
